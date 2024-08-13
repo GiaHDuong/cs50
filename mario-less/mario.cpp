79668abb31eeeba2edd57ad06bda06accc40d1bc
Gia Duong <giaduong5906@gmail.com>
2024-08-12 23:17:22 -0400

    first commit

---
+++ mario-less/mario.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int height;
+
+    cout << "Height: ";
+    cin >> height;
+
+    for (int i=0; i<=height; i++) {
+        string space(height-i, ' ');
+        string brick(i, '#');
+        cout << space << brick << "\n";
+    }
+}
